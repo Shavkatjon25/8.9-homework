1f1363a71969be7f1f4e20d77f926b6dff04f995
Shavkatjon25 <shavkatjona75@gmail.com>
2024-05-25 09:25:23 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+import Mn from "./components/Mn"
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Mn/>
+    </div>
+  )
+}
+
+export default App
+
